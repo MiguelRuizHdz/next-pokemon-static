a2af734feeeee7575a20304a94b97c0506d40233
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-04-28 16:05:04 -0500

    NextUI - React UI Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next'
 
+import { Button } from '@nextui-org/react'
+
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">
+        Hola Mundo
+      </Button>
     </>
   )
 }
