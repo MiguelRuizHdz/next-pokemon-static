66e6ebcddd4f886c50c56d9014018c1be9818597
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-09 13:40:04 -0500

    Next - GetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,41 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 
 import { Button } from '@nextui-org/react'
 import { Layout } from '../componentes/layouts'
 
 // title: Listado de Pokémons
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log(props);
+
   return (
     <Layout title='Listado de Pokémons'>
-      <Button color="gradient">
-        Hola Mundo
-      </Button>
+      
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
+
     </Layout>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  
+  console.log('Hola Mundo');
+
+  return {
+    props: {
+      name: 'Miguel'
+    }
+  }
 }
 
 export default HomePage
