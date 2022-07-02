cf85d02d3f96b063dd59742287a68df06214b8d6
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-07-02 17:35:37 -0500

    Mejorar la lectura de la página

---
+++ componentes/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from 'react'
+import { Card, Grid } from '@nextui-org/react';
+import { FavoriteCardPokemons } from './';
+
+interface Props {
+    pokemons: number[];
+}
+
+export const FavoritePokemons: FC<Props> = ({  pokemons })=> {
+  return (
+    <Grid.Container gap={ 2 } direction='row' justify='flex-start' >
+    {
+        pokemons.map( id => (
+            <FavoriteCardPokemons key={ id } pokemonId={ id } />
+        ))
+    }
+    </Grid.Container>
+  )
+}
