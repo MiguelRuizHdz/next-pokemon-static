4a5725d1ead0767657e058f3fa2110b8c5e716db
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-07-02 20:00:26 -0500

    Open Graph Meta Tags

--- componentes/layouts/Layout.tsx
+++ componentes/layouts/Layout.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
 import { FC, ReactNode } from 'react'
+import Head from 'next/head'
+
 import { Navbar } from '../ui'
 
 interface Props {
@@ -7,7 +8,12 @@ interface Props {
     title?: string
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
+
+  console.log({origin});
+  
   return (
     <>
         <Head>
@@ -15,6 +21,11 @@ export const Layout: FC<Props> = ({ children, title }) => {
             <meta name='author' content='Miguel Ruiz' />
             <meta name='description' content={`Información sobre el pokémon ${ title }`} />
             <meta name='keywords' content={`${ title }, pokemon, pokedex`} />
+
+            <meta property="og:title" content={`Información sobre ${ title }`} />
+            <meta property="og:description" content={`Esta es la página sobre ${ title }`} />
+            <meta property="og:image" content={`${ origin }/img/banner.png`} />
+            
         </Head>
 
         {/* Navbar */}
